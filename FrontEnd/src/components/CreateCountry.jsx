31f5f6b6acccd3871386262de420f2442ef87243
messiron <brayancordoba242@gmail.com>
2024-02-15 21:23:59 -0500

    Agregando funcionalidades del crud en la visual

--- FrontEnd/src/components/CreateCountry.jsx
+++ FrontEnd/src/components/CreateCountry.jsx
@@ -1,51 +1,83 @@
 
 import './CreateCountry.css'
+import {useState} from 'react'
 
 
 export const CreateCountry = () => {
+  const [ok , setOk] = useState(false)
+  function HandleSubmit(e){
+    e.preventDefault()
+    const data = {
+      code:e.target.code.value,
+      name:e.target.name.value,
+      language:e.target.name.value,
+      continent:e.target.continent.value
+      
+    }
+   const URL = 'http://127.0.0.1:3000/Insert'
+   const settings = {
+    method:'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+   }
+  fetch(URL, settings)
+   .then(res=>{
+    if(res.ok){
+      setOk(true)
+
+    }
+   })
+  .catch(err=>console.log(err))
+  }
   return (
+
     <div className="containerCreate">
-      <div  className='Create'>
-      <label htmlFor="miInput">Codigo del Pais:</label>
-      <input type="text" id="miInput" name="miInput" />
-      <button type="button"  id='query'>Consultar</button>
+      <div className='Create'>
+        <label htmlFor="miInput">Codigo del Pais:</label>
+        <input type="text" id="miInput" name="codeInput" />
+        <button type="button" id='query'>Consultar</button>
+        
       </div>
+      {ok && <p>Pais Creado Correctamente</p>}
+
+      <form onSubmit={HandleSubmit}>
+        <div className='codName'>
+          <div>
+            <label htmlFor="miInput">Codigo:</label>
+            <input type="text" id="miInput" name="code" />
+          </div>
+
+          <div>
+            <label htmlFor="miInput">Nombre:</label>
+            <input type="text" id="miInput" name="name" />
+          </div>
+
+        </div>
+
+        <div className='Lenguage'>
+          <div>
+            <label htmlFor="miInput">Lengua:</label>
+            <input type="text" id="miInput" name="language" />
+          </div>
+
+          <div>
+            <label htmlFor="miInput">Continente:</label>
+            <input type="text" id="miInput" name="continent" />
+          </div>
+
+        </div>
 
 
-  <div  className='codName'>
-      <div>
-      <label htmlFor="miInput">Codigo:</label>
-      <input type="text" id="miInput" name="miInput" />
-      </div>
+        <div className='butons'>
+          <div>
+            <button id='buton1'>Limpiar</button>
+            <button id='buton2' type='submit'>Crear</button>
+          </div>
 
-      <div>
-      <label htmlFor="miInput">Nombre:</label>
-      <input type="text" id="miInput" name="miInput" />
-      </div>
-
-      </div>
-
-      <div  className='Lenguage'>
-      <div>
-      <label htmlFor="miInput">Lengua:</label>
-      <input type="text" id="miInput" name="miInput" />
-      </div>
-
-      <div>
-      <label htmlFor="miInput">Continente:</label>
-      <input type="text" id="miInput" name="miInput" />
-      </div>
-
-      </div>
-
-
-   <div  className='butons'>
-      <div>
-      <button id='buton1'>Limpiar</button>
-      <button id='buton2'>Crear</button>
-      </div>
-
-      </div>
+        </div>
+      </form>
 
 
 
--- FrontEnd/src/components/ManageCountry.jsx
+++ FrontEnd/src/components/ManageCountry.jsx
@@ -1,70 +1,108 @@
 
 
 import './ManageCountry.css';
-
+import { useState } from 'react';
 const ManageCountry = () => {
+  const [data, setData] = useState({
+    code: "",
+    name: "",
+    language: "",
+    continent: "",
+    consulta:"",
+    _id:""
+
+  })
+  const [ok , setOk] = useState(false)
+  const consultar = function(){
+    const URL = "http://127.0.0.1:3000/countryByCode/" + data.consulta
+    fetch(URL)
+    .then(res=>res.json())
+    .then(data=>{
+      setData({
+        code:data.code,
+        name:data.name,
+        continent:data.continent,
+        language:data.language,
+        consulta:data.code,
+        id:data._id
+
+      })
+    })
+    .catch(err => console.log(err))
+
+  }
+  const Delete = function(){
+    const URL = "http://127.0.0.1:3000/countries/" + data.id
+    fetch(URL,{method:"DELETE"})
+    .then(res=>{
+      if(res.ok){
+        setOk(true)
+        
+      }
+    })
+  }
   return (
     <div className="containerManage">
-       <div  className='codigo'>
-      <label htmlFor="miInput">Codigo del Pais:</label>
-      <input type="text" id="miInput" name="miInput" />
-      <button type="button" id='quer'>Consultar</button>
+      <div className='codigo'>
+        <label htmlFor="miInput">Codigo del Pais:</label>
+        <input type="text" id="miInput" name="miInput" value={data.consulta} onChange={e =>setData({...data,consulta:e.target.value})} />
+        <button type="button" id='quer'onClick={consultar}>Consultar </button>
       </div>
 
 
-<div className='inputts'>
+      <div className='inputts'>
+          {ok && <p>Eliminado</p>}
+
+        <div className='capital'>
+          <div>
+            <label htmlFor="miInput">Codigo :</label>
+            <input
+              type="text"
+              id="miInput"
+              name="miInput"
+              value={data.code}
+              onChange={e => setData({ ...data, code: e.target.value })} />
+          </div>
+
+          <div>
+            <label htmlFor="miInput"> name:</label>
+            <input type="text"
+              id="miInput"
+              name="miInput"
+              value={data.name}
+              onChange={e => setData({ ...data, name: e.target.value })} />
+          </div>
+
+          <div className='continente' >
+            <div>
+              <label htmlFor="miInput">continente:</label>
+              <input type="text"
+                id="miInput"
+                name="miInput"
+                value={data.continent}
+                onChange={e => setData({ ...data, continent: e.target.value })} />
+            </div>
+
+            <div>
+              <label htmlFor="miInput">language:</label>
+              <input type="text" id='Conti' name="miInput" value={data.language} onChange={e => setData({ ...data, language: e.target.value})} />
+            </div>
 
 
-<div className='capital'>
-  <div>
-<label htmlFor="miInput">Codigo :</label>
- <input type="text" id="miInput" name="miInput" />
- </div>
+            
+          </div>
 
- <div>
-<label htmlFor="miInput"> Lengua:</label>
-  <input type="text" id="miInput" name="miInput" />
-  </div>
+        </div>
 
+        <div className='updates'>
+          <div>
+            <button id='buton1' onClick={Delete}>Eliminar</button>
+            <button id='buton2'>Actualizar</button>
+          </div>
 
-<div>
-<label htmlFor="miInput">Capital:</label>
-  <input type="text" id="miInput" name="miInput" />
-  </div>
-</div>
+        </div>
 
-
-
-
-<div className='continente' >
-<div>
-<label htmlFor="miInput">Nombre:</label>
- <input type="text" id="miInput" name="miInput" />
- </div>
-
- <div>
-<label htmlFor="miInput">Continente :</label>
-  <input type="text"   id='Conti' name="miInput" />
-  </div>
-
-
-<div>
-<label htmlFor="miInput">Moneda:</label>
-  <input type="text" id="miInput" name="miInput" />
-  </div>
-</div>
-
-    </div>
-
-    <div  className='updates'>
-      <div>
-      <button id='buton1'>Eliminar</button>
-      <button id='buton2'>Actualizar</button>
       </div>
-
-      </div>
-
-
 
 
     </div>
