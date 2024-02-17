58ac89a57e73e102470cd0211db29dec5e50a94b
ivan4322 <138176845+ivan4322@users.noreply.github.com>
2024-02-17 16:19:27 -0500

    funcion filtrar por continente

--- FrontEnd/src/components/Home.jsx
+++ FrontEnd/src/components/Home.jsx
@@ -1,63 +1,89 @@
-import './Home.css'
-import { useEffect, useState } from "react"
+import './Home.css';
+import ContinentFilter from './ContinentFilter';
+import Africa from'../assets/AFRICA.jpg'
+import Europa from'../assets/EUROPA.jpg'
+import Asia from'../assets/ASIA.jpg'
+import Oceania from'../assets/OCEANIA.jpg'
+import America from'../assets/AMERICA.png'
 
 
-
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  const [countries, setCountries] = useState([])
-  const [searchTerm, setSearchTerm] = useState('')
+    const [countries, setCountries] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [selectedContinent, setSelectedContinent] = useState('');
+    const [inputClicked, setInputClicked] = useState(false);
 
-  useEffect(() => {
-      fetch("http://127.0.0.1:3000/countries")
-          .then(res => res.json())
-          .then(data => setCountries(data))
-  }, [])
+    useEffect(() => {
+        fetch("http://127.0.0.1:3000/countries")
+            .then(res => res.json())
+            .then(data => setCountries(data));
+    }, []);
 
-  const handleSearch = (event) => {
-      setSearchTerm(event.target.value)
-  }
+    const handleContinentChange = (continent) => {
+        setSelectedContinent(continent);
+        setInputClicked(false);
+    };
 
-  const filteredCountries = countries.filter(country =>
-      country.name.toLowerCase().includes(searchTerm.toLowerCase())
-  )
+    const handleInputClick = () => {
+        setInputClicked(true);
+    };
 
-  return (
-      <div className="Home">
-     
-          <div className="search-bar">
-           
-           <div className='search'>
-           <div  className='searchstyle' >
-            <div className='count'>
-            <span id='pais' >Pais</span>
-           <input 
-                  type="text"
-                 placeholder='escriba el pais que desea ver'
-                  className='buscador'
-                  value={searchTerm}
-                  onChange={handleSearch}
-              />
-               </div>
-              <span id='lupa'>Buscar</span>
-              </div>
-           </div>
-             
+    const handleInputChange = (event) => {
+        setSearchTerm(event.target.value);
+        setInputClicked(false);
+    };
 
-              <div className='countries'>
-              <h1>Countries</h1>
-              </div>
-              <section className='Cards'>
-                  {filteredCountries.map((country) => (
-                      <div key={country._id} className="content">
-                          <img src={`https://flagsapi.com/${country.code}/flat/64.png`} alt="flag" />
-                          <h1>{country.name}</h1>
-                      </div>
-                  ))}
-              </section>
-          </div>
-      </div>
-  );
+    const continentMappings = {
+        "África": { name: "África", continents: ["Africa"], image: Africa },
+        "América": { name: "América", continents: ["South America", "North America"], image: America },
+        "Asia": { name: "Asia", continents: ["Asia"], image: Asia },
+        "Europa": { name: "Europa", continents: ["Europe"], image: Europa },
+        "Oceanía": { name: "Oceanía", continents: ["Oceania"], image: Oceania }
+    };
+
+    const filteredCountries = countries.filter(country =>
+        country.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
+        (selectedContinent === '' || continentMappings[selectedContinent].continents.includes(country.continent))
+    );
+
+    return (
+        <div className="Home">
+            <div className="search-bar">
+                <div className='search'>
+                    <div className='searchstyle'>
+                        <div className='count'>
+                            <span id='pais'>Pais</span>
+                            <input
+                                type="text"
+                                placeholder='escriba el pais que desea ver'
+                                className='buscador'
+                                value={searchTerm}
+                                onChange={handleInputChange}
+                                onClick={handleInputClick}
+                            />
+                        </div>
+                        <span id='lupa'>Buscar</span>
+                    </div>
+                </div>
+               
+                {inputClicked && searchTerm === '' && 
+                    <ContinentFilter continentMappings={continentMappings} onContinentChange={handleContinentChange} />}
+                <div className='countries'>
+                   
+                </div>
+                <section className='Cards'>
+                    {filteredCountries.map((country) => (
+                        <div key={country._id} className="content">
+                            <img src={`https://flagsapi.com/${country.code}/flat/64.png`} alt="flag" />
+                            <h1>{country.name}</h1>
+                        </div>
+                    ))}
+                </section>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
